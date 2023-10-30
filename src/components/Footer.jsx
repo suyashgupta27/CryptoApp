a3a2250120160dedec39d7904342b3a6c27571b8
suyash gupta <1suyashgupta@gmail.com>
2023-10-30 10:18:06 +0530

    second

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,7 @@
 import { Avatar, Box, Stack, Text, VStack } from "@chakra-ui/react";
 import React from "react";
 import img from "../assets/1.jpeg";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
@@ -24,7 +25,9 @@ const Footer = () => {
         </VStack>
 
         <VStack>
-          <Avatar boxSize={"28"} mt={["4", "0"]} src={img} />
+          <Link to="https://www.linkedin.com/in/suyashgupta27/" target="_blank">
+            <Avatar boxSize={"28"} mt={["4", "0"]} src={img} />
+          </Link>
           <Text>Our Founder</Text>
         </VStack>
       </Stack>
